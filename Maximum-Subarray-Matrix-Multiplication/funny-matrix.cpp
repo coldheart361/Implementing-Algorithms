756ae24188ffeb478173c3b1271f7dc6e523d9ba
coldheart361 <satya.maulana.adigama@gmail.com>
2025-11-12 15:10:35 +0800

    done with funny matrix

--- Maximum-Subarray-Matrix-Multiplication/funny-matrix.cpp
+++ Maximum-Subarray-Matrix-Multiplication/funny-matrix.cpp
@@ -5,6 +5,17 @@ using namespace std;
 // in this implementation, we assume the matrices are square and powers of 2 so that we do
 // not have to deal with messy stuff
 
+int power(int x, int y) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 1) {
+        return x * power(x, y/2);
+    } else {
+        return x * power(x, y/2);
+    }
+}
+
 int absolute_value(int a) {
     if (a < 0) {
         return -a;
@@ -18,11 +29,19 @@ int maxn = 256;
 class Matrix {
     public:
     vector<vector<int>> entries;
-    int R, C;
+    int R, C, maxVal;
     Matrix(vector<vector<int>> matrix) {
         this->entries = matrix;
         R = matrix.size();
         C = matrix[0].size();
+        int curr = 0;
+        for (int i = 0; i < R; i += 1) {
+            for (int j = 0; j < C; j += 1) {
+                if (absolute_value(matrix[i][j]) > curr) {
+                    curr = absolute_value(matrix[i][j]) 
+                }
+            }
+        }
     } 
     Matrix add(Matrix matrix) {
         vector<vector<int>> ans;
@@ -63,6 +82,23 @@ class Matrix {
         }
         return Matrix(ans);
     }
+    Matrix merge(Matrix C11, Matrix C12, Matrix C21, Matrix C22) {
+        vector<vector<int>> ans;
+        for (int i = 0; i < 2*C11.R; i += 1) {
+            ans.push_back(vector<int> {});
+            for (int j = 0; j < 2*C11.C; i += 1) {
+                if (i < C11.R && j < C11.R) {
+                    ans[i].push_back(C11.entries[i][j]);
+                } else if (i >= C11.R && j < C11.R) {
+                    ans[i - C11.R].push_back(C21.entries[i - C11.R][j]);
+                } else if (i < C11.R && j >= C11.R) {
+                    ans[i].push_back(C12.entries[i][j - C11.R]);
+                } else {
+                    ans[i - C11.R].push_back(C21.entries[i - C11.R][j - C11.R]);
+                }
+            }
+        }
+    } 
     Matrix Strassen_Multiply(Matrix A, Matrix B) {
 // Create Matrix objects for A and B for easier operation chaining
 
@@ -119,8 +155,43 @@ class Matrix {
     }
 
     // this one's a bit funky, but we implement the funny matrix multiplication as in [3] of the 
-    // paper     
+    // paper. Here is the part that doesn't really make sense for normal computers to do.  
+    Matrix psi(Matrix matrix) {
+        vector<vector<int>> ans;
+        for (int i = 0; i < matrix.R; i += 1) {
+            ans.push_back(vector<int> {});
+            for (int j = 0; j < matrix.R; j += 1) {
+                ans[i].push_back(power(matrix.R + 1, matrix.maxVal - matrix.entries[i][j]));
+            }
+        }
+    }
     Matrix Funny_Multiply(Matrix A, Matrix B) {
-        
+        // we are working under the assumption that A and B are nice matrices
+       Matrix psiA = psi(A); 
+       Matrix psiB = psi(B);
+       Matrix res = Strassen_Multiply(psiA, psiB);
+       vector<vector<int>> ans;
+       // it doesn't make sense to do this but okay
+       for (int i = 0; i < res.R; i += 1) {
+        ans.push_back(vector<int> {});
+            for (int j = 0; j < res.R; j += 1) {
+                int high = max(A.maxVal, B.maxVal); 
+                int low = 0;
+                int mid = (high - low) / 2;
+                int checker = power(res.R, mid);
+                while (low < high) {
+                    if (res.entries[i][j] % checker == 0) {
+                        low = mid + 1;
+                    } else {
+                        high = mid;
+                    }
+                    int mid = (high - low) / 2;
+                    checker = power(res.R, mid);
+                    ans[i].push_back(mid);
+                }
+                // thus we get the maximum divisor and hence the maximum sum of entries a_i,j + b_i,j
+            }
+       }
+       return Matrix(ans);
     }
 }
