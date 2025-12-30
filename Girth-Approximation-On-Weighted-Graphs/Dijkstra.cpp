75ea589a3dca92877e4196d19024753a5eaa0ec2
coldheart361 <satya.maulana.adigama@gmail.com>
2025-12-31 01:05:51 +0700

    refined the class

--- Girth-Approximation-On-Weighted-Graphs/Dijkstra.cpp
+++ Girth-Approximation-On-Weighted-Graphs/Dijkstra.cpp
@@ -1,18 +1,13 @@
+
 #include "Graph.h"
+#include "Dijkstra.h"
 #include <vector>
 #include <queue>
 #include <unordered_map>
 #include <limits>
 using namespace std;
 
-struct PairHasher {
-    template <class T1, class T2>
-    size_t operator()(const pair<T1, T2>& p) const {
-        auto h1 = hash<T1>{}(p.first);
-        auto h2 = hash<T2>{}(p.second);
-        return h1 ^ (h2 + 0x9e3779b9 + (h1 << 6) + (h1 >> 2));
-    }
-};
+
 
 const double INF = numeric_limits<double>::infinity();
 // assuming source and target are inside 
--- Girth-Approximation-On-Weighted-Graphs/Dijkstra.h
+++ Girth-Approximation-On-Weighted-Graphs/Dijkstra.h
@@ -1,4 +1,15 @@
+#ifndef DIJKSTRA
+#define DIJKSTRA
 #include "Graph.h"
 using namespace std;
 pair<double, node*> Dijkstra(graph&, node*, node*);
 pair<double, node*> ModifiedDijkstra(graph&, node*, node*);
+struct PairHasher {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const {
+        auto h1 = hash<T1>{}(p.first);
+        auto h2 = hash<T2>{}(p.second);
+        return h1 ^ (h2 + 0x9e3779b9 + (h1 << 6) + (h1 >> 2));
+    }
+};
+#endif
